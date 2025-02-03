66b961731dc8cc775688fa006839870865cb02d0
Hermon Tesfatsion <64483255+hermontes@users.noreply.github.com>
2025-02-03 11:31:06 -0500

    Add frontend and backend directories

---
+++ clients/src/components/BlogPost.js
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import CreateComments from "./CreateComments";
+import CommentCards from "./CommentCards";
+
+import "./styling/ArticleContent.css";
+
+const SingleBlog = ({ blog }) => {
+  //sorting them by date
+  blog.comments.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+  const [viewComments, setView] = useState(false);
+
+  const dateBlogCreated = new Date(blog.date).toLocaleDateString("en-US", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
+  const timeBlogCreated = new Date(blog.date).toLocaleTimeString("en-US", {
+    hour: "numeric",
+    minute: "numeric",
+    hour12: true,
+  });
+
+  const changeViewComment = () => {
+    setView(!viewComments);
+  };
+
+  return (
+    <div className="contentCard">
+      <h1 className="blogTitle">
+        <span>{blog.title}</span>
+      </h1>
+      <div className="authorAndDate">
+        <span className="blogAuthor">By {blog.author} </span>{" "}
+        <div className="blogDate">
+          {" "}
+          {dateBlogCreated} at {timeBlogCreated}
+        </div>
+      </div>
+
+      <img className="blogImage" src={blog.image} alt=""></img>
+      
+      <p className="contentText">{blog.content}</p>
+
+      <div className="viewCommentsButton">
+        <button onClick={changeViewComment} value={viewComments}>
+          {viewComments ? "Hide Comments" : "View Comments"}
+        </button>
+      </div>
+
+      {viewComments ? (
+        <div className="commentSection">
+          <h2>Comments</h2>
+          <hr />
+          <CreateComments blogPost={blog} />
+          <CommentCards blog={blog} />
+        </div>
+      ) : (
+        ""
+      )}
+
+      <hr className="separator"></hr>
+    </div>
+  );
+};
+
+export default SingleBlog;
---
+++ clients/src/components/CreateComments.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { useState } from "react";
+import Axios from "axios";
+
+import "./styling/CreateComments.css";
+// import './App.css';
+
+const Comments = ({ blogPost }) => {
+  const [commentField, setComment] = useState("");
+  const [name, setName] = useState("");
+
+  const createComment = () => {
+    Axios.put("http://localhost:3001/makeComment", {
+      title: blogPost.title,
+      comments: { name: name, text: commentField,likeCount:0,dislikeCount:0 },
+    });
+    // //clearing the values after submission
+    setName("");
+    setComment("");
+  };
+
+  return (
+    <div className="createCommentsSection">
+      <div className="commentHeading">
+        {blogPost.comments.length == 0
+          ? blogPost.comments.length + " comments, be the first one!"
+          : blogPost.comments.length + " comments, join the conversation!"}
+      </div>
+
+      <input
+        type="text"
+        className="commentInput"
+        value={name}
+        placeholder="Full Name..."
+        onChange={(e) => setName(e.target.value)}
+      ></input>
+
+      <div>
+        <textarea
+          className="commentBox"
+          value={commentField}
+          placeholder="Share your thoughts..."
+          onChange={(e) => setComment(e.target.value)}
+        ></textarea>
+      </div>
+      <div className="buttonAlign">
+        <button onClick={createComment}>Comment</button>
+      </div>
+    </div>
+  );
+};
+
+export default Comments;
