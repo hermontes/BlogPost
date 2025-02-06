3f33676f325707a237011ca32bb0adc53e785e26
Hermon Tesfatsion <64483255+hermontes@users.noreply.github.com>
2025-02-06 17:09:17 -0500

    comment

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const app = express();
+const app = express(); //initializes an Express application.
 const cors = require("cors");
 const PostsStructure = require("./models/PostsStructure");
 require('dotenv').config();
