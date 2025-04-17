d08d60d659c6b05e5c98337dfc284be41750f1b2
Hermon Tesfatsion <64483255+hermontes@users.noreply.github.com>
2025-04-17 10:52:28 -0400

    Handle comment submission logic gracefully and set time limit on status

--- clients/src/components/CreateComments.js
+++ clients/src/components/CreateComments.js
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 import Axios from "axios";
 
 import "./styling/CreateComments.css";
+import "./styling/SharedStyles.css";
 
-
-const Comments = ({ blogPost}) => {
+const Comments = ({ blogPost }) => {
   const [commentField, setComment] = useState("");
   const [name, setName] = useState("");
+  const [formError, setFormError] = useState(false);
+  const [statusMessage, setStatusMessage] = useState("");
 
-  const createComment = () => {
+  const ERROR_MESSAGE =
+    "An error occured while submitting your comment, please try again";
+  const SUCCESS_MESSAGE = "Successfully created a comment!";
+
+  const areFieldsFilled = commentField.trim() && name.trim();
+
+  const createComment = (event) => {
+    event.preventDefault();
+    if (name === "testError") {
+      setFormError(true);
+      setStatusMessage(ERROR_MESSAGE);
+
+      return Promise.reject(new Error("Simulated error"));
+    }
     Axios.put("http://localhost:3001/makeComment", {
       _id: blogPost._id,
       comments: {
@@ -18,49 +33,79 @@ const Comments = ({ blogPost}) => {
         likeCount: 0,
         dislikeCount: 0,
       },
-    }).catch((error) => {
-      console.error("Error creating comment:", error);
-
-    });
-    // //clearing the values after submission
-    setName("");
-    setComment("");
-
+    })
+      .then(() => {
+        // //clearing the values after submission
+        setName("");
+        setComment("");
+        setFormError(false);
+        setStatusMessage(SUCCESS_MESSAGE);
+      })
+      .catch((error) => {
+        setFormError(true);
+        setStatusMessage(ERROR_MESSAGE);
+        console.error("Error creating comment:", error);
+      });
   };
+
+  //clearing the submit status message after 3 seconds
+  useEffect(() => {
+    //set timeout and clean it up after
+    if(statusMessage) {
+      const timer = 
+      setTimeout(() => setStatusMessage(""), 3000)
+      return () => clearTimeout(timer)
+    }
+  }, [statusMessage])
+
 
   return (
     <>
-    <div className="createCommentsSection">
-      <div className="commentHeading">
-        {blogPost.comments.length == 0
-          ? blogPost.comments.length + " comments, be the first one!"
-          : blogPost.comments.length + " comments, join the conversation!"}
-      </div>
+      <div className="createCommentsSection">
+        <div className="commentHeading">
+          {blogPost.comments.length == 0
+            ? blogPost.comments.length + " comments, be the first one!"
+            : blogPost.comments.length + " comments, join the conversation!"}
+        </div>
 
-      <input
-        type="text"
-        className="commentInput"
-        value={name}
-        placeholder="Full Name..."
-        onChange={(e) => setName(e.target.value)}
-        maxLength={50}
-        required
-      ></input>
+        <form onSubmit={createComment}>
+          <div className="create-comment-fields">
+            <input
+              type="text"
+              className="commentInput"
+              value={name}
+              placeholder="Full Name..."
+              onChange={(e) => setName(e.target.value)}
+              maxLength={50}
+              required
+            ></input>
 
-      <div>
-        <textarea
-          className="commentBox"
-          value={commentField}
-          placeholder="Share your thoughts..."
-          onChange={(e) => setComment(e.target.value)}
-          maxLength={2000}
-          required
-        ></textarea>
+            <textarea
+              className="commentBox"
+              value={commentField}
+              placeholder="Share your thoughts..."
+              onChange={(e) => setComment(e.target.value)}
+              maxLength={2000}
+              required
+            ></textarea>
+          </div>
+          <div>
+            {/* display this section, only if status message is not empty */}
+            {statusMessage && (
+              <div
+                className={`submission-status  ${
+                  formError ? "error" : "success"
+                }`}
+              >
+                {statusMessage}
+              </div>
+            )}
+          </div>
+          <div className="buttonAlign">
+            <button type="submit"> Comment</button>
+          </div>
+        </form>
       </div>
-      <div className="buttonAlign">
-        <button onClick={createComment}>Comment</button>
-      </div>
-    </div>
     </>
   );
 };
