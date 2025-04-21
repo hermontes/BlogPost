c700390a09361fa6ce625054b907b11c401d9898
Hermon Tesfatsion <64483255+hermontes@users.noreply.github.com>
2025-04-21 19:53:26 -0400

    Display the immediate like or dislike count change
    Use state to track local list of comments

--- clients/src/components/CommentCards.js
+++ clients/src/components/CommentCards.js
@@ -1,43 +1,66 @@
-import { React, useMemo } from "react";
+import { React, useEffect, useMemo, useState } from "react";
 import Axios from "axios";
 import "./styling/CommentCards.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faThumbsUp, faThumbsDown } from "@fortawesome/free-solid-svg-icons"; // Import icons
 
 const CommentCards = ({ blog, formatDateAndTimeFunction }) => {
+
+  const [comments, setComments] = useState(blog.comments);
+
+  //when blog.comments changes, update the local list of comments too
+  useEffect(() => {
+    setComments(blog.comments)
+  }, [blog.comments])
+
   const sortedComments = useMemo(() => {
-    return [...blog.comments].sort(
+    return [...comments].sort(
       (a, b) => new Date(b.createdAt) - new Date(a.createdAt)
     );
-  }, [blog.comments]);
+  }, [comments]);
 
   // Updating like and dislike counts
-  const handleLikeAndDislikes = (type, commentId, currentLikesOrDislikes) => {
-    Axios.put("http://localhost:3001/updateLikeOrDislike", {
+  const handleLikeAndDislikes = (type, commentId, currentCount) => {
+
+    Axios.put(`${process.env.REACT_APP_API_URL}/updateLikeOrDislike`, {
       id: blog._id,
       commentInfo: {
         id: commentId,
         type: type,
-        currentLikesOrDislikes: currentLikesOrDislikes,
+        currentLikesOrDislikes: currentCount,
       },
     })
-      .catch((error) => {
-        console.log("received back an error: ", error);
-      })
-      .then((response) => {
-        if (response) {
-          console.log("RECEIVED BACK: ", response.data.comments[0]);
-        }
+    .catch((error) => {
+      console.log("received back an error: ", error);
+    })
+    .then((response) => {
+      if (response) {
+        console.log("RECEIVED BACK: ", response.data.comments[0]);
+      }
+
+      //Display the immediate like or dislike count change
+      const updatedComment = response.data.comments[0];
+      setComments((prevComments) => {
+        return prevComments.map(comment => {
+          if (comment._id === updatedComment._id) {
+            return updatedComment
+          } else{
+            return comment;
+          }
+        });
       });
+
+    });
+
   };
 
   return (
     <div className="comments-container">
       {sortedComments.length === 0
         ? ""
-        : sortedComments.map((comment, index) => {
+        : sortedComments.map((comment) => {
             return (
-              <div className="comment-card" key={index}>
+              <div className="comment-card" key={comment._id}>
                 <div className="comment-left">
                   <img
                     src="/defaultUser.png"
@@ -63,8 +86,7 @@ const CommentCards = ({ blog, formatDateAndTimeFunction }) => {
                             "like",
                             comment._id,
                             comment.likeCount
-                          )
-                        }
+                          )                        }
                       >
                         <FontAwesomeIcon icon={faThumbsUp} />
                         <span>{comment.likeCount}</span>
