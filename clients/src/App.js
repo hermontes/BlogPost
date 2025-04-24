6c074ed445f9f0e9d699a6d163b8efddb364822e
Hermon Tesfatsion <64483255+hermontes@users.noreply.github.com>
2025-04-23 23:47:43 -0400

    Remove unused code

--- clients/src/App.js
+++ clients/src/App.js
@@ -94,10 +94,6 @@ function App() {
     };
   }, []);
 
-  // const sortedContent =() => {
-  //   return [...allContent].sort((a, b) => new Date(b.date) - new Date(a.date));
-  // };
-
   return (
     // <div className="App">
     <div className="container" ref={createContentRef}>
